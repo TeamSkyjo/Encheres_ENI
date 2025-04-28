b2c29a945165b459ca9afb0f555d3939f09feed2
Adrien HUHN <adrien.huhn2025@campus-eni.fr>
2025-04-28 14:08:52 +0200

    first commit

---
+++ src/main/java/fr/tp/eni/encheresskyjo/EncheresSkyjoApplication.java
@@ -0,0 +1,13 @@
+package fr.tp.eni.encheresskyjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresSkyjoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncheresSkyjoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/tp/eni/encheresskyjo/EncheresSkyjoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tp.eni.encheresskyjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresSkyjoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
