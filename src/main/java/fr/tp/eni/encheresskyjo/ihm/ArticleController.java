a153e6671a83320eab64c4704f2be689e9bbcf74
Adrien HUHN <adrien.huhn2025@campus-eni.fr>
2025-05-02 16:45:59 +0200

    Added view for article details

--- src/main/java/fr/tp/eni/encheresskyjo/ihm/ArticleController.java
+++ src/main/java/fr/tp/eni/encheresskyjo/ihm/ArticleController.java
@@ -2,10 +2,12 @@ package fr.tp.eni.encheresskyjo.ihm;
 
 import fr.tp.eni.encheresskyjo.bll.ArticleService;
 import fr.tp.eni.encheresskyjo.bo.Article;
+import fr.tp.eni.encheresskyjo.bo.ArticleStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,6 +36,16 @@ public class ArticleController {
         model.addAttribute("articles", articles);
         System.out.println("Page d'accueil");
         return "index";
+    }
+
+    @GetMapping("/article")
+    public String displayArticle(
+            @RequestParam(name="id", required = true) int id,
+            Model model
+    ) {
+        Article article =  this.articleService.getArticleById(id);
+        model.addAttribute("article", article);
+        return "article/details";
     }
 
 }
