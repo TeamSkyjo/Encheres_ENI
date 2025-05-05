c899dfc0dcda3194d98329f5ea9ff13c20c53d52
SutefaniD <110696367+SutefaniD@users.noreply.github.com>
2025-05-05 12:07:13 +0200

    IHM: view link on seller and display profile

--- src/main/java/fr/tp/eni/encheresskyjo/ihm/UserController.java
+++ src/main/java/fr/tp/eni/encheresskyjo/ihm/UserController.java
@@ -1,9 +1,11 @@
 package fr.tp.eni.encheresskyjo.ihm;
 
 import fr.tp.eni.encheresskyjo.bll.UserService;
+import fr.tp.eni.encheresskyjo.dto.UserGeneralDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -20,8 +22,18 @@ public class UserController {
         return "/register";
     }
 
+    /**
+     * map to another user's profile WHEN USER IS LOGGED IN.
+     * @param username
+     * @param model
+     * @return
+     */
     @GetMapping("/profil")
-    public String displayUserProfile(Model model) {
+    public String displayUserProfile(
+            @RequestParam(name="username", required=true) String username,
+            Model model) {
+        UserGeneralDTO user = this.userService.loadUser(username);
+        model.addAttribute("user", user);
 
         return "/user/profile";
     }
